839af77f3d58a66545463cb5da9b914c66ee039b
Dacd <2415276803@qq.com>
2022-06-22 17:29:50 +0800

    master 3ths commit

--- src/main/java/test/pop/GitTest.java
+++ src/main/java/test/pop/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("热维修1");
         System.out.println("master的改变");
         System.out.println("hot-fix下的改变");
+        System.out.println("咕咕咕");
     }
 }
